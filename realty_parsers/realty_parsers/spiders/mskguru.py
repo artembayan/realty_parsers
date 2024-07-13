342fce70cdd75ac9cbc32618d3b15badbeef6ed0
Артем Бабаян <a.babaian@brandquad.ru>
2024-07-13 15:39:55 +0300

    add new developers spider and crawling price per metr to buildings spider

--- realty_parsers/realty_parsers/spiders/mskguru.py
+++ realty_parsers/realty_parsers/spiders/mskguru.py
@@ -42,7 +42,7 @@ class MskguruParser:
         return {'title': title}
 
     def get_price(self, response=None):
-        price = response.xpath(PP_PRICE).get('')
+        price = response.xpath(PP_PRICE).get('').strip()
         if price != 'Цена по запросу':
             price_range = price.split('–')
             price_range = [int(re.sub(r'\D', '', price)) for price in price_range]
@@ -51,6 +51,17 @@ class MskguruParser:
         else:
             price_min = price_max = None
         return {'price_min': price_min, 'price_max': price_max}
+
+    def get_price_per_metr(self, response=None):
+        price = response.xpath(PP_PRICE_PER_METR).get('').strip()
+        if price != 'Узнавайте по телефону':
+            price_range = price.split('–')
+            price_range = [int(re.sub(r'\D', '', price)) for price in price_range]
+            price_min = min(price_range)
+            price_max = max(price_range)
+        else:
+            price_min = price_max = None
+        return {'price_per_metr_min': price_min, 'price_per_metr_max': price_max}
 
     def get_status(self, response=None):
         status = response.xpath(PP_DATE).get('')
@@ -86,6 +97,38 @@ class MskguruParser:
         description = self.clear_string(description)
         return {'description': description}
 
+    def get_devs_urls(self, response):
+        urls = response.xpath(PL_DEVELOPERS).getall()
+        return urls
+
+    def get_dev_name(self, response=None):
+        name = self.clear_string(response.xpath(DP_NAME).get(''))
+        return {'name': name}
+
+    def get_dev_logo(self, response=None):
+        logo = response.xpath(DP_LOGO).get('')
+        return {'logo': logo}
+
+    def get_dev_foundation_year(self, response=None):
+        foundation_year = response.xpath(DP_FOUNDATION_YEAR).get('')
+        return {'year': foundation_year}
+
+    def get_dev_rented_qnty(self, response=None):
+        rented = response.xpath(DP_RENTED_QNTY).get("0")
+        rented_qnty = re.search("(\d+)", rented).group(0)
+        return {'rented_qnty': int(rented_qnty)}
+
+    def get_dev_being_built_qnty(self, response=None):
+        being_built = response.xpath(DP_BEING_BUILT_QNTY).get("0")
+        being_built_qnty = re.search("(\d+)", being_built).group(1)
+        return {'being_built_qnty': int(being_built_qnty)}
+
+    def get_dev_description(self, response=None):
+        description = response.xpath(DP_DESCRIPTION).getall()
+        description = ''.join(description)
+        description = self.clear_string(description)
+        return {'description': description}
+
 
 class MskguruSpider(Spider, MskguruParser):
     name = 'mskguru.ru'
@@ -106,7 +149,10 @@ class MskguruSpider(Spider, MskguruParser):
             'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 750,
             'scrapy.downloadermiddlewares.retry.RetryMiddleware': 550,
             'scrapy.downloadermiddlewares.downloadtimeout.DownloadTimeoutMiddleware': 350,
-        }
+        },
+        # 'EXTENSIONS': {
+        #     'realty_parsers.middlewares.UploadItems': 800
+        # },
     }
 
     def start_requests(self) -> Iterable[Request]:
@@ -142,6 +188,7 @@ class MskguruSpider(Spider, MskguruParser):
             result.update(self.get_url(response))
             result.update(self.get_title(response))
             result.update(self.get_price(response))
+            result.update(self.get_price_per_metr(response))
             result.update(self.get_status(response))
             result.update(self.get_date(response))
             result.update(self.get_location(response))
@@ -150,5 +197,60 @@ class MskguruSpider(Spider, MskguruParser):
             result.update(self.get_parameters(response))
             result.update(self.get_description(response))
             yield result
-        except:
+        except Exception as e:
             self.log(f'Parse Item Err: DataOut {response.url}')
+
+
+class MskgurDevelopersSpider(Spider, MskguruParser):
+    name = 'mskguru.ru_developers'
+    allowed_domains = ['mskguru.ru']
+    start_urls = ['https://mskguru.ru/companies']
+    custom_settings = {
+        'DOWNLOAD_TIMEOUT': 60,
+        'DOWNLOAD_DELAY': 0.7,
+        'CONCURRENT_REQUESTS': 8,
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 8,
+        'AUTOTHROTTLE_TARGET_CONCURRENCY': 10,
+        'REFERER_ENABLED': False,
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'RETRY_TIMES': 10,
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 750,
+            'scrapy.downloadermiddlewares.retry.RetryMiddleware': 550,
+            'scrapy.downloadermiddlewares.downloadtimeout.DownloadTimeoutMiddleware': 350,
+        },
+        'ITEM_PIPELINES': {
+            'realty_parsers.pipelines.DropDuplicateItems': 100,
+        },
+        'UNIQUE_KEYS': ['name', 'logo'],
+        # 'EXTENSIONS': {
+        #     'realty_parsers.middlewares.UploadItems': 800
+        # },
+    }
+
+    def parse(self, response):
+        try:
+            urls = self.get_devs_urls(response)
+            self.log(f"Got {len(urls)} urls from {response.url}")
+            for url in urls:
+                yield Request(url=url, callback=self.parse_developer, dont_filter=True,
+                              meta={'proxy': PROXY_URL})
+            next_page = self.get_next_page(response)
+            if next_page:
+                yield Request(url=next_page, callback=self.parse, dont_filter=True,
+                              meta={'proxy': PROXY_URL})
+        except:
+            self.log(f"Last list page: {response.url}")
+
+    def parse_developer(self, response):
+        result = {}
+        try:
+            result.update(self.get_dev_name(response))
+            result.update(self.get_dev_logo(response))
+            result.update(self.get_dev_foundation_year(response))
+            result.update(self.get_dev_rented_qnty(response))
+            result.update(self.get_dev_being_built_qnty(response))
+            result.update(self.get_dev_description(response))
+        except Exception as e:
+            self.log(f'Parse Item Err: DataOut {response.url}')
+        yield result
